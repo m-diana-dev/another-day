050fbe160c7dcef711a9dfebe07b8e0b7c6423a8
m-diana-dev <m.diana.dev@gmail.com>
2022-10-24 17:08:16 +0300

    added branch dev and start project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Tasks from './Tasks';
+import Water from './Water';
 
 const App = () => {
   return (
     <div className="App">
-
+      <div className="AppContainer">
+        <Tasks />
+        <Water />
+      </div>
     </div>
   );
 }
